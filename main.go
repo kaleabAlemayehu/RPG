711ead060c181149bd7a155bfd71128a4131cf43
neoCipher (Kaleab Alemayehu) <cipherlofi04@gmail.com>
2025-01-05 08:24:14 -0500

    adding movement

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"image"
 	"image/color"
 	"log"
 
@@ -8,15 +9,32 @@ import (
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
-type Game struct{}
+type Game struct {
+	PlayerImage *ebiten.Image
+	X, Y        float64
+}
 
 func (g *Game) Update() error {
+	if ebiten.IsKeyPressed(ebiten.KeyUp) || ebiten.IsKeyPressed(ebiten.KeyK) {
+		g.Y -= 2
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyDown) || ebiten.IsKeyPressed(ebiten.KeyJ) {
+		g.Y += 2
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyRight) || ebiten.IsKeyPressed(ebiten.KeyL) {
+		g.X += 2
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyLeft) || ebiten.IsKeyPressed(ebiten.KeyH) {
+		g.X -= 2
+	}
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	screen.Fill(color.RGBA{120, 180, 255, 255})
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+	opts := ebiten.DrawImageOptions{}
+	opts.GeoM.Translate(g.X, g.Y)
+	screen.DrawImage(g.PlayerImage.SubImage(image.Rect(0, 0, 16, 16)).(*ebiten.Image), &opts)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -27,7 +45,11 @@ func main() {
 	ebiten.SetWindowSize(640, 480)
 	ebiten.SetWindowTitle("Hello, World!")
 	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	eImg, _, err := ebitenutil.NewImageFromFile("./assets/ninjaDark.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := ebiten.RunGame(&Game{PlayerImage: eImg, X: 100.0, Y: 200.0}); err != nil {
 		log.Fatal(err)
 	}
 }
